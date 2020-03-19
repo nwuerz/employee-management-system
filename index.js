7d33b29e9377c32de032ae6cd1d4e9a591d196ea
nick wuerz <nawuerz@gmail.com>
2020-03-18 23:15:31 -0500

    added  viewAll and dptView functions

--- index.js
+++ index.js
@@ -26,21 +26,102 @@ function start() {
             choices: [
                 "View All Employees",
                 "View All Employees By Department",
-                "View All Employees By Manager"
+                "View All Employees By Manager",
+                "Remove Employee",
+                "Update Employee Role",
+                "Update Employee Manager",
+                "View All Roles",
+                "Create A Role",
+                "Remove A Role",
+                "View Department Budget"
             ]
         }
-    ]).then((answer)=> {
-        switch(userAction)
+    ]).then((answer) => {
+        switch(answer.userAction)
         {
-            case (answer.userAction === "View All Employees"):
+            case "View All Employees":
                 viewAll();
                 break;
-            case (answer.userAction === "View All Employees By Department"):
+            case "View All Employees By Department":
                 dptView();
                 break;
-            case (answer.userAction === "View All Employees by Manager"):
+            case "View All Employees by Manager":
                 mgrView();
+                break;
+            case "Remove Employee":
+                removeEmployee();
+                break;
+            case "Update Employee Role":
+                updateRole();
+                break;
+            case "Update Employee Manager":
+                updateMgr();
+                break;
+            case "View All Roles":
+                viewRoles();
+                break;
+            case "Create A Role":
+                createRole();
+                break;
+            case "Remove A Role":
+                removeRole();
+                break;
+            case "View Department Budget":
+                viewBudget();
+                break;
         }
     });
 }
 
+function viewAll() {
+    connection.query("SELECT id, first_name, last_name FROM employees", function(err, res) {
+        if (err) throw err;
+        var values = res.map(function(employee){
+            return [employee.id, employee.first_name, employee.last_name];
+        });
+        console.table(['Id', 'First Name', 'Last Name'], values);
+            start();
+    });
+}
+
+function dptView() {
+    connection.query("SELECT id, first_name, last_name, department FROM employees ORDER BY department", function(err, res) {
+        if (err) throw err;
+        var values = res.map(function(employee){
+            return [employee.id, employee.first_name, employee.last_name, employee.department];
+        });
+        console.table(['Id', 'First Name', 'Last Name', 'Department'], values);
+    });
+}
+
+// function mgrView() {
+
+// }
+
+// function removeEmployee() {
+
+// }
+
+// function updateRole() {
+
+// }
+
+// function updateMgr() {
+
+// }
+
+// function viewRoles() {
+
+// }
+
+function createRole() {
+    
+}
+
+// function removeRole() {
+
+// }
+
+// function viewBudget() {
+
+// }
