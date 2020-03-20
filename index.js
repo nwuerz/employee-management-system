1486253e5a120a6560c2629d8e61358e6ec43e0a
nick wuerz <nawuerz@gmail.com>
2020-03-20 15:57:28 -0500

    started work on addEmployee function

--- index.js
+++ index.js
@@ -25,15 +25,16 @@ function start() {
             name: "userAction",
             choices: [
                 "View All Employees",
-                "View All Employees By Department",
-                "View All Employees By Manager",
-                "Remove Employee",
-                "Update Employee Role",
-                "Update Employee Manager",
+                "View All Departments",
                 "View All Roles",
-                "Create A Role",
-                "Remove A Role",
-                "View Department Budget"
+                "Add An Employee"
+                // "Remove Employee",
+                // "Update Employee Role",
+                // "Update Employee Manager",
+                // "View All Roles",
+                // "Create A Role",
+                // "Remove A Role",
+                // "View Department Budget"
             ]
         }
     ]).then((answer) => {
@@ -48,27 +49,30 @@ function start() {
             case "View All Roles":
                 viewRoles();
                 break;
+            case "Add an Employee":
+                addEmployee();
+                break;
             // case "View All Employees by Manager":
             //     mgrView();
             //     break;
-            case "Remove Employee":
-                removeEmployee();
-                break;
-            case "Update Employee Role":
-                updateRole();
-                break;
-            case "Update Employee Manager":
-                updateMgr();
-                break;
-            case "Create A Role":
-                createRole();
-                break;
-            case "Remove A Role":
-                removeRole();
-                break;
-            case "View Department Budget":
-                viewBudget();
-                break;
+            // case "Remove Employee":
+            //     removeEmployee();
+            //     break;
+            // case "Update Employee Role":
+            //     updateRole();
+            //     break;
+            // case "Update Employee Manager":
+            //     updateMgr();
+            //     break;
+            // case "Create A Role":
+            //     createRole();
+            //     break;
+            // case "Remove A Role":
+            //     removeRole();
+            //     break;
+            // case "View Department Budget":
+            //     viewBudget();
+            //     break;
         }
     });
 }
@@ -91,6 +95,7 @@ function dptView() {
             return [department.id, department.department_name];
         });
         console.table(['Id', 'Department Name'], values);
+        start();
     });
 }
 
@@ -101,29 +106,46 @@ function viewRoles() {
             return [roles.id, roles.title, roles.salary, roles.deparment_id];
         });
         console.table(['Id', 'Title', 'Salary', 'Department Id'], values);
+        start();
     });
 }
 
-// function removeEmployee() {
-
-// }
-
-// function updateRole() {
-
-// }
-
-// function updateMgr() {
-
-// }
-
-// function createRole() {
-    
-// }
-
-// function removeRole() {
-
-// }
-
-// function viewBudget() {
-
-// }
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "firstName",
+            message: "What is the new employee's first name?"
+        },
+        {
+            type: "input",
+            name: "lastName",
+            message: "What is the new employee's last name?"
+        },
+        {
+            type: "list",
+            message: "What is the new employee's role?",
+            name: "role",
+            choices: [
+                "Software Engineer",
+                "Lead Engineer",
+                "Accountant",
+                "Lawyer",
+                "Legal Team Lead",
+                "Salesperson",
+                "Sales Lead"
+            ]
+        },
+        {
+            type: "list",
+            message: "Please assign new employee to a manager:",
+            name: "manager",
+            choices: [
+                "Ashley Rodriguez",
+                "John Doe",
+                "Mike Chan",
+                "Sarah Lourd"
+            ]
+        }
+    ]).then()
+}
